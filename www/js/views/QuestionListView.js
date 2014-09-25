e41af87e54d85b3be1bf9eb677c91f0a7d7e7ead
markengelnmac <markengelnmac@sparemac01s-MacBook-Air.local>
2014-09-25 16:25:46 -0700

    question titles with sevenDayAgoFunction variables

--- www/js/views/QuestionListView.js
+++ www/js/views/QuestionListView.js
@@ -4,6 +4,10 @@ var QuestionListView = Backbone.View.extend({
 	render: function(){
 		console.log("QuestionListView");
 		$(this.el).html("");
-		$(this.el).html(this.template(this.model.toJSON()));	
+		var rawTitle = this.model.get("title");
+		var date = new Date();
+		var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][(date.getDay()+1) % 7];
+		var title = rawTitle.replace("sevenDaysAgoFunction", day);
+		$(this.el).html(this.template({"title": title}));	
 	}
 });
