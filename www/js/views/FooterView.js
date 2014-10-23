85826516bdb01225fee6252191e99daa4daa6181
Paul Smith <pauls@sccwrp.org>
2014-10-23 13:38:22 -0700

    restart popup

--- www/js/views/FooterView.js
+++ www/js/views/FooterView.js
@@ -6,13 +6,24 @@ var FooterView = Backbone.View.extend({
 	},
     	events: {
 		"click #back": "back",
-		"click #forward": "forward"
+		"click #forward": "forward",
+		"click #restart": "restart",
+		"click #restartCancel": "cancel",
+		"click #restartRestart": "reload"
 	},
     	back: function (e) { 
 		this.trigger("back");
 	},
     	forward: function (e) { 
 		this.trigger("forward");
+	},
+    	restart: function (e) { 
+	  e.preventDefault();
+	  $("#popupRestart").html( new RestartView().render().el );
+	  //$("#popupRestart").popup({ create: function(e,u){ alert("create"); }});
+	  $("#popupRestart").trigger("create");
+	  $("#popupRestart").popup("open");
+	  //$("#popupRestart").on({ "popupafteropen", function(event,ui){ alert(event); alert(ui); }});
 	},
 	render: function(){
 		console.log("footer");
