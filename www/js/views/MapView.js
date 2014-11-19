398b1894a281269fb1d68c1610aa75ea9e9f291c
Paul Smith <pauls@sccwrp.org>
2014-11-19 10:54:04 -0800

    update q21 and finished map

--- www/js/views/MapView.js
+++ www/js/views/MapView.js
@@ -5,15 +5,10 @@ var MapView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		console.log("map");
-		var map = new google.maps.Map(document.getElementById("map-canvas-test"), { zoom: 12, center: {lat: 32.7663694, lng: -117.2592576} });
+		var map = new google.maps.Map(document.getElementById("map-canvas"), { zoom: 12, center: {lat: 32.7663694, lng: -117.2592576} });
 		var report = [];
-		//var ReportName = [];
-		//var ReportCast = [];
-		//var surfReport = [{id: 1,title: "WINDANSEA",surf: "", latlng: "32.828746,242.719325"},{id: 2, title: "TOURMALINE",surf: "", latlng: "32.828746,242.719325"}];
-		var surfReport = {};
-		surfReport[0] = ['WINDANSEA','32.828746,242.719325'];
-		surfReport[1] = ['TOURMALINE','32.828746,242.719325'];
+		var ReportName = [];
+		var ReportCast = [];
 		$("#landing").hide();
 		$(headerView.el).show();
 		url = 'http://feeds.feedburner.com/surfline-rss-surf-report-south-san-diego?format=xml';
@@ -28,57 +23,37 @@ var MapView = Backbone.View.extend({
 			},
 			success: function(xml){
 			        values = xml.responseData.feed.entries;
-				//console.log(values);
-				//console.log(surfReport[0].id);
 				for(var i=0;i<16;i++){
 					report = xml.responseData.feed.entries[i].title.split(':');
-					console.log(surfReport[0].indexOf(report[0].trim()));
-					//console.log(report[0].trim());
-					//console.log(surfReport[0][0]);
-					//if(surfReport[0][0] === report[0]){
-						//console.log("yes");
-					//}
-					//reportSpot = report.shift();
-					//console.log(reportSpot);
-					//ReportSpot.push(reportSpot);
-					//ReportReport.push(report);
+					reportTitle = report.shift();
+					ReportName.push(reportTitle);
+					ReportCast.push(report);
 				}
-				//console.log(ReportName);
-				//console.log(ReportCast);
-				//console.log(ReportCast[ReportName.indexOf('WINDANSEA ')]);
-				return report;
+	  			placeMarker("32.828746","242.719325","Windansea",ReportCast[ReportName.indexOf('WINDANSEA ')]);
+	  			placeMarker("32.813788","242.727917","Birdrock",ReportCast[ReportName.indexOf('BIRDROCK ')]);
+	  			placeMarker("32.794057","242.743063","Pacific Beach",ReportCast[ReportName.indexOf('PACIFIC BEACH ')]);
+	  			placeMarker("32.771904","242.746682","Mission Beach",ReportCast[ReportName.indexOf('MISSION BEACH ')]);
+	  			placeMarker("32.751839","242.746854","Ocean Beach",ReportCast[ReportName.indexOf('OCEAN BEACH, SD ')]);
+	  			placeMarker("32.727039","242.743728","Sunset Cliffs",ReportCast[ReportName.indexOf('SUNSET CLIFFS ')]);
+	  			placeMarker("32.804964","242.737635","Tourmaline",ReportCast[ReportName.indexOf('OLD MAN\'S/TOURMALINE ')]);
 			}
 	    	});
-		console.log(report);
-		var mylatlng = new google.maps.LatLng(32.828746,242.719325);
-	  	placeMarker(mylatlng,"windandsea","2-3ft");
-		function placeMarker(location,title,surf){
-			console.log("placeMarker");
-			console.log(location);
+		function placeMarker(lat,lng,title,surf){
+			var mylatlng = new google.maps.LatLng(lat,lng);
 			var marker = new google.maps.Marker({
-				position: location,
+				position: mylatlng,
 			    	map: map,
+			    	icon: 'img/surf_icon40.png'
 			})
 			var infowindow = new google.maps.InfoWindow({
-				//content: 'Latitude: ' + location.lat() +
-			 	//'<br>Longitude: ' + location.lng()
 				content: 'Spot: ' + title +
 				'<br>Surf: ' + surf 
 		    	});
-		    	infowindow.open(map,marker);
+		    	//infowindow.open(map,marker);
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker);
+		    	});
 		}
-		//.bindPopup("<b>Windansea</b><br />" + ReportCast[ReportName.indexOf('WINDANSEA ')] + "").openPopup();
-		//$('#question').html("Map");
-		//$(this.el).html("");
-		//$(this.el).html(this.template());	
-		/*
-		map = new google.maps.Map(document.getElementById("map-canvas-test"), { zoom: 12, center: {lat: 32.7663694, lng: -117.2592576} });
-		map.data.loadGeoJson('surfspots.json');
-	 	map.data.addListener ('click', function(event) {
-	      		document.getElementById ('info-box').textContent =
-		  	event.feature.getProperty('url');
-		}); 
-		*/
 		return this;
 	}
 });
