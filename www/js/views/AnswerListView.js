c30ad1840f49ba079b4addbd87d8351b0198c81c
Paul Smith <pauls@sccwrp.org>
2014-12-18 07:40:19 -0800

    fix for radio button double click

--- www/js/views/AnswerListView.js
+++ www/js/views/AnswerListView.js
@@ -206,6 +206,10 @@ var AnswerListView = Backbone.View.extend({
 		var appID;
 		var that = this;
 		formtype = this.model.get("type");
+		// disable radio button double click
+                if(formtype == "radio"){
+		        $(".ui-radio").css("pointer-events", "none");
+		}
 		if(other) {
 			var currentAnswer = other;
 		} else if(!decline) {
